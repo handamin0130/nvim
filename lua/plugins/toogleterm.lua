075bcb3d862f410a9726c0f800018a21a06d0469
handamin0130 <handamin0130@gmail.com>
2024-05-24 13:59:17 +0900

    feat: add keymap of toggleterm

--- lua/plugins/toogleterm.lua
+++ lua/plugins/toogleterm.lua
@@ -1,8 +1,12 @@
+local map_key = require("utils.key_mapper").map_key
+
 return {
 	{
 		"akinsho/toggleterm.nvim",
 		config = function()
 			require("toggleterm").setup({})
+
+			map_key("<leader>t", ":ToggleTerm<CR>")
 		end,
 	},
 }
