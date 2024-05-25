d1e5623569ba16548f0864d5ac30ae24839a727b
handamin0130 <handamin0130@gmail.com>
2024-05-25 13:14:56 +0900

    feat: add js, ts lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,59 +1,62 @@
 local map_key = require("utils.key_mapper").map_key
 
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", -- lua
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls", -- lua
 
-                    "clangd", -- c, c++
-                    "rust_analyzer", -- rust
+					"vtsls", -- js, ts
 
-                    "wgsl_analyzer", -- wgsl
+					"clangd", -- c, c++
+					"rust_analyzer", -- rust
 
-                    "markdown_oxide", "mdx_analyzer",
-                    -- md, mdx
+					"wgsl_analyzer", -- wgsl
 
-                    "taplo", --toml
-                    "yamlls", --yaml
-                    "jsonls", -- json
-                }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
+					"markdown_oxide",
+					"mdx_analyzer",
+					-- md, mdx
 
-            lspconfig.lua_ls.setup({})
+					"taplo", --toml
+					"yamlls", --yaml
+					"jsonls", -- json
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.clangd.setup({})
-            lspconfig.rust_analyzer.setup({})
+			lspconfig.lua_ls.setup({})
 
-            lspconfig.wgsl_analyzer.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.rust_analyzer.setup({})
 
-            lspconfig.markdown_oxide.setup({})
-            lspconfig.mdx_analyzer.setup({})
+			lspconfig.wgsl_analyzer.setup({})
 
-            lspconfig.taplo.setup({})
-            lspconfig.yamlls.setup({})
-            lspconfig.jsonls.setup({})
+			lspconfig.markdown_oxide.setup({})
+			lspconfig.mdx_analyzer.setup({})
 
-            -- vim.lsp.buf.hover
-            -- vim.lsp.buf.definition
-            -- vim.lsp.buf.code_action
-            map_key("K", vim.lsp.buf.hover)
-            map_key("gd", vim.lsp.buf.definition)
-            map_key("<leader>ca", vim.lsp.buf.code_action)
-        end
-    }
+			lspconfig.taplo.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.jsonls.setup({})
+
+			-- vim.lsp.buf.hover
+			-- vim.lsp.buf.definition
+			-- vim.lsp.buf.code_action
+			map_key("K", vim.lsp.buf.hover)
+			map_key("gd", vim.lsp.buf.definition)
+			map_key("<leader>ca", vim.lsp.buf.code_action)
+		end,
+	},
 }
